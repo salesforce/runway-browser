c1620712054b7d403bbe48f5501959a14716fa11
Diego Ongaro <dongaro@salesforce.com>
2015-10-01 19:11:26 -0700

    Run make format (no manual changes)

--- parser.js
+++ parser.js
@@ -4,11 +4,11 @@ var input = fs.readFileSync('input.model.js').toString();
 
 // like .mark() except puts start and end in same object as value
 Parsimmon.Parser.prototype.source = function() {
-    return Parsimmon.Parser.prototype.mark.call(this).map(function(marked) {
-        marked.value.start = marked.start;
-        marked.value.end = marked.end;
-        return marked.value;
-    });
+  return Parsimmon.Parser.prototype.mark.call(this).map(function(marked) {
+    marked.value.start = marked.start;
+    marked.value.end = marked.end;
+    return marked.value;
+  });
 }
 
 
@@ -23,13 +23,12 @@ var string = Parsimmon.string;
 var whitespace = Parsimmon.whitespace;
 
 var sepByOptTrail = function(content, separator) {
-    return sepBy(content, separator)
-            .skip(separator.or(Parsimmon.succeed()));
+  return sepBy(content, separator)
+    .skip(separator.or(Parsimmon.succeed()));
 }
-
 var comment = regex(/\/\/[^\n]*/).desc('single-line comment');
 function lexeme(p) {
-    return p.skip(whitespace.or(comment).many());
+  return p.skip(whitespace.or(comment).many());
 }
 
 var arrow = lexeme(string('->'));
@@ -50,232 +49,257 @@ var times = lexeme(string('*'));
 
 var re = /([0-9]+)([a-z]+)/i;
 var numberWithUnit = lexeme(regex(re))
-                        .map(function(s) {
-                            var result = re.exec(s);
-                            return {
-                                kind: 'numberWithUnit',
-                                number: result[1],
-                                unit: result[2],
-                        }; })
-                        .desc('number with unit').mark();
+  .map(function(s) {
+    var result = re.exec(s);
+    return {
+      kind: 'numberWithUnit',
+      number: result[1],
+      unit: result[2],
+    };
+  })
+  .desc('number with unit').mark();
 var number = lexeme(regex(/[0-9]+/).map(parseInt)).desc('number').mark();
 var id = lexeme(regex(/[a-z_]\w*/i)).desc('identifier').mark();
 
 var atom = numberWithUnit.or(number).or(id);
-var group = lazy(function() { return lparen.then(expr).skip(rparen) });
+var group = lazy(function() {
+  return lparen.then(expr).skip(rparen)
+});
 var binop = alt(times, plus, minus);
 var expr = alt(seqMap(atom,
-                      binop,
-                      atom,
-                      function(left, op, right) { return {
-                        kind: 'apply',
-                        func: op,
-                        args: [left, right]
-                      }; }),
-               atom,
-               group)
-                 .desc('expression');
+  binop,
+  atom, function(left, op, right) {
+    return {
+      kind: 'apply',
+      func: op,
+      args: [left, right]
+    };
+  }),
+  atom,
+  group)
+  .desc('expression');
 
-var range = seqMap(expr, dots, expr,
-                   function(low, _, high) { return {
-                        kind: 'range',
-                        low: low,
-                        high: high
-                   }; }).source();
+var range = seqMap(expr, dots, expr, function(low, _, high) {
+  return {
+    kind: 'range',
+    low: low,
+    high: high
+  };
+}).source();
 
-var field = lazy(function() { return seqMap(
-                id,
-                colon,
-                complexType.or(type),
-                function(id, _, type) { return {
-                    id: id,
-                    type: type
-                }; });
-            });
+var field = lazy(function() {
+  return seqMap(
+    id,
+    colon,
+    complexType.or(type), function(id, _, type) {
+      return {
+        id: id,
+        type: type
+      };
+    });
+});
 
 
 var fieldlist = sepByOptTrail(field, comma);
 
 var node = lexeme(string('node'))
-            .skip(lbrace)
-            .then(fieldlist.map(function(fields) {
-                return { kind: 'record',
-                         fields: fields }; }))
-            .skip(rbrace);
+  .skip(lbrace)
+  .then(fieldlist.map(function(fields) {
+    return {
+      kind: 'record',
+      fields: fields
+    };
+  }))
+  .skip(rbrace);
 
 var eitherfield = seqMap(id,
-                         lbrace,
-                         fieldlist,
-                         rbrace,
-                         function(id, _, fields, _2) { return {
-                            id: id,
-                            fields: fields,
-                            kind: 'record',
-                         }; }).or(id);
+  lbrace,
+  fieldlist,
+  rbrace, function(id, _, fields, _2) {
+    return {
+      id: id,
+      fields: fields,
+      kind: 'record',
+    };
+  }).or(id);
 var eitherfieldlist = sepByOptTrail(eitherfield, comma);
 
 var either = lexeme(string('either'))
-            .skip(lbrace)
-            .then(eitherfieldlist.map(function(fields) {
-                return { kind: 'either',
-                         fields: fields }; }))
-            .skip(rbrace);
+  .skip(lbrace)
+  .then(eitherfieldlist.map(function(fields) {
+    return {
+      kind: 'either',
+      fields: fields
+    };
+  }))
+  .skip(rbrace);
 var generic = seqMap(id,
-                     langle,
-                     id,
-                     rangle,
-                     function(base, _, arg, _2) { return {
-                        kind: 'generic',
-                        base: base,
-                        args: [arg],
-                     }; });
+  langle,
+  id,
+  rangle, function(base, _, arg, _2) {
+    return {
+      kind: 'generic',
+      base: base,
+      args: [arg],
+    };
+  });
 var type = alt(range, generic, id);
 
 var complexType = Parsimmon.alt(
-                    node,
-                    either);
+  node,
+  either);
 
 var param = seqMap(lexeme(string('param')),
-                   id,
-                   colon,
-                   type,
-                   equals,
-                   expr,
-                   semicolon,
-                   function(_, id, _2, type, _3, value, _4) { return {
-                       kind: 'paramdecl',
-                       id: id,
-                       type: type,
-                       default: value
-                   }; });
+  id,
+  colon,
+  type,
+  equals,
+  expr,
+  semicolon, function(_, id, _2, type, _3, value, _4) {
+    return {
+      kind: 'paramdecl',
+      id: id,
+      type: type,
+      default: value
+    };
+  });
 
 var typedecl = seqMap(lexeme(string('type')),
-                      id,
-                      colon,
-                      complexType.or(type.skip(semicolon)),
-                      function(_, id, _2, type) { return {
-                        kind: 'typedecl',
-                        id: id,
-                        type: type,
-                      }; });
+  id,
+  colon,
+  complexType.or(type.skip(semicolon)), function(_, id, _2, type) {
+    return {
+      kind: 'typedecl',
+      id: id,
+      type: type,
+    };
+  });
 
 var vardecl = lexeme(string('var'))
-                .then(id)
-                .skip(colon)
-                .then(type)
-                .skip(semicolon);
+  .then(id)
+  .skip(colon)
+  .then(type)
+  .skip(semicolon);
 
-var block = lazy(function() { return lbrace
-                                        .then(statement.many())
-                                        .skip(rbrace); });
+var block = lazy(function() {
+  return lbrace
+    .then(statement.many())
+    .skip(rbrace);
+});
 
 var arg = seqMap(id,
-                 colon,
-                 type,
-                 function(id, _, type) { return {
-                    id: id,
-                    type: type,
-                 }; });
+  colon,
+  type, function(id, _, type) {
+    return {
+      id: id,
+      type: type,
+    };
+  });
 
 var arglist = lparen
-                .then(sepBy(arg, comma))
-                .skip(rparen);
+  .then(sepBy(arg, comma))
+  .skip(rparen);
 
 var distribution = seqMap(lexeme(string('distribution')),
-                          id,
-                          arglist,
-                          arrow,
-                          type,
-                          block,
-                          function(_, id, args, _2, returntype, block) { return {
-                               kind: 'distributiondecl',
-                               id: id,
-                               args: args,
-                               returntype: returntype,
-                               code: block
-                          }; });
+  id,
+  arglist,
+  arrow,
+  type,
+  block, function(_, id, args, _2, returntype, block) {
+    return {
+      kind: 'distributiondecl',
+      id: id,
+      args: args,
+      returntype: returntype,
+      code: block
+    };
+  });
 
 var returnStmt = lexeme(string('return'))
-                    .then(expr).map(function(v) { return {
-                        kind: 'returnstmt',
-                        value: v, }; })
-                    .skip(semicolon);
+  .then(expr).map(function(v) {
+  return {
+    kind: 'returnstmt',
+    value: v,
+  };
+})
+  .skip(semicolon);
 
 
 var statement = Parsimmon.alt(
-                    param,
-                    typedecl,
-                    distribution,
-                    returnStmt,
-                    vardecl).source();
+  param,
+  typedecl,
+  distribution,
+  returnStmt,
+  vardecl).source();
 
 // main parser
-var file = lazy(function() { return lexeme(string('')).then(statement.many()); });
+var file = lazy(function() {
+  return lexeme(string('')).then(statement.many());
+});
 
 var parse = function(input) {
-    var r = file.parse(input);
-    r.input = input;
-    return r;
+  var r = file.parse(input);
+  r.input = input;
+  return r;
 }
-
 var parseFile = function(filename) {
-    return parse(fs.readFileSync(filename).toString());
+  return parse(fs.readFileSync(filename).toString());
 }
-
 var consoleOutput = function(parseResult) {
-    var r = parseResult;
-    var input = r.input;
-    if (r.status) {
-        console.log(JSON.stringify(r.value, null, 2));
+  var r = parseResult;
+  var input = r.input;
+  if (r.status) {
+    console.log(JSON.stringify(r.value, null, 2));
+  } else {
+    console.log('Parsing failed');
+    var startsAt = input.lastIndexOf("\n", r.index);
+    if (startsAt == -1) {
+      startsAt = r.index;
     } else {
-        console.log('Parsing failed');
-        var startsAt = input.lastIndexOf("\n", r.index);
-        if (startsAt == -1) {
-            startsAt = r.index;
-        } else {
-            startsAt += 1;
-        }
-        var endsAt = input.indexOf("\n", r.index);
-        var lineno = 1;
-        var nl = - 1;
-        while (true) {
-            var nl = input.indexOf("\n", nl + 1);
-            if (nl == -1 || nl > r.index)
-                break;
-            lineno += 1;
-        }
-        console.log('line %d: %s', lineno, input.slice(startsAt, endsAt));
-        var w = '';
-        for (var i = 0; i < r.index - startsAt + 7 + lineno.toString().length; i += 1) {
-            w += ' ';
-        }
-        console.log('%s^', w);
-        //console.log('Starting:', input.slice(r.index, endsAt));
-
-        var unique = [];
-        r.expected.forEach(function(v) {
-            if (unique.indexOf(v) == -1) {
-                unique.push(v);
-            }
-        });
-        unique.sort();
-        var exp = '';
-        unique.forEach(function(v, i) {
-            exp += v.toString();
-            if (i < unique.length - 1) {
-                exp += ', ';
-            }
-        });
-        console.log('expected:', exp);
+      startsAt += 1;
     }
+    var endsAt = input.indexOf("\n", r.index);
+    var lineno = 1;
+    var nl = -1;
+    while (true) {
+      var nl = input.indexOf("\n", nl + 1);
+      if (nl == -1 || nl > r.index) {
+        break;
+      }
+      lineno += 1;
+    }
+    console.log('line %d: %s', lineno, input.slice(startsAt, endsAt));
+    var w = '';
+    for (var i = 0; i < r.index - startsAt + 7 + lineno.toString().length; i += 1) {
+      w += ' ';
+    }
+    console.log('%s^', w);
+    //console.log('Starting:', input.slice(r.index, endsAt));
+
+    var unique = [];
+    r.expected.forEach(function(v) {
+      if (unique.indexOf(v) == -1) {
+        unique.push(v);
+      }
+    });
+    unique.sort();
+    var exp = '';
+    unique.forEach(function(v, i) {
+      exp += v.toString();
+      if (i < unique.length - 1) {
+        exp += ', ';
+      }
+    });
+    console.log('expected:', exp);
+  }
 };
 
 module.exports = {
-    parse: parse,
-    parseFile: parseFile,
-    consoleOutput: consoleOutput,
+  parse: parse,
+  parseFile: parseFile,
+  consoleOutput: consoleOutput,
 };
 
 if (require.main === module) {
-    consoleOutput(parseFile('input.model.js'));
+  consoleOutput(parseFile('input.model.js'));
 }
