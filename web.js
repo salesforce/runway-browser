6a78af918167403f319d551d586f725a0277e96d
Diego Ongaro <dongaro@salesforce.com>
2016-02-05 17:47:32 -0800

    Move Controller into separate file

--- web.js
+++ web.js
@@ -24,7 +24,7 @@ let StateDump = require('./web/statedump.jsx');
 let RuleControls = require('./web/rulecontrols.jsx');
 let ExecutionView = require('./web/executionview.jsx');
 let REPLView = require('./web/repl.jsx');
-let Changesets = require('./changesets.js');
+let Controller = require('./controller.js');
 
 let preludeText = require('./prelude.model');
 
@@ -97,134 +97,6 @@ let fetchRemoteJSX = function(filename) {
       return module.exports;
     });
 };
-
-class SerializedState {
-  constructor(state) {
-    this.state = state;
-  }
-  toJSON() {
-    return this.state;
-  }
-  toString() {
-    return JSON.stringify(this.state, null, 2);
-  }
-  equals(other) {
-    return _.isEqual(this.state, other.state);
-  }
-}
-
-class Controller {
-  constructor(module) {
-    this.module = module;
-    this.views = [];
-    this.execution = [];
-    this.execution.push({
-      msg: 'Initial state',
-      state: this.serializeState(),
-      index: 0,
-    });
-    this.checkInvariants();
-  }
-
-  checkInvariants() {
-    try {
-      this.module.env.invariants.forEachLocal((invariant, name) => {
-        let context = {};
-        invariant.check(context);
-      });
-    } catch ( e ) {
-      if (e instanceof errors.Runtime) {
-        let msg = `Failed invariant ${name}: ${e}`;
-        console.log(msg);
-        jQuery('#error').text(msg);
-      } else {
-        throw e;
-      }
-    }
-  }
-
-  serializeState() {
-    let state = {};
-    this.module.env.vars.forEachLocal((mvar, name) => {
-      if (!mvar.isConstant) {
-        state[name] = mvar.toJSON();
-      }
-    });
-    return new SerializedState(state);
-  }
-
-  restoreState(state) {
-    state = state.toJSON();
-    this.module.env.vars.forEachLocal((mvar, name) => {
-      if (!mvar.isConstant) {
-        mvar.assignJSON(state[name]);
-      }
-    });
-  }
-
-  tryChangeState(mutator) {
-    jQuery('#error').text('');
-    this.checkInvariants();
-    let oldState = this.serializeState();
-    let msg = mutator();
-    if (msg === undefined) {
-      msg = 'state changed';
-    }
-    let newState = this.serializeState();
-    if (oldState.equals(newState)) {
-      return false;
-    } else {
-      msg += ' (changed ' + Changesets.compareJSON(oldState.toJSON(), newState.toJSON()).join(', ') + ')';
-      console.log(msg);
-      this.execution.push({
-        msg: msg,
-        state: newState,
-        index: this.execution.length,
-      });
-      this.checkInvariants();
-      this.updateViews();
-      return true;
-    }
-  }
-
-  wouldChangeState(mutator) {
-    let oldState = this.serializeState();
-    mutator();
-    let newState = this.serializeState();
-    if (oldState.equals(newState)) {
-      return false;
-    } else {
-      this.restoreState(oldState);
-      return true;
-    }
-  }
-
-  resetToStartingState() {
-    this.tryChangeState(() => {
-      console.log('reset');
-      jQuery('#error').text('');
-      this.module.env.vars.forEachLocal((mvar, name) => {
-        mvar.assign(mvar.type.makeDefaultValue());
-      });
-      let context = {};
-      this.module.ast.execute(context); // run global initialization code
-      return 'reset';
-    });
-  }
-
-  restore(snapshot) {
-    console.log('restore');
-    jQuery('#error').text('');
-    this.execution = this.execution.slice(0, snapshot.index + 1);
-    this.restoreState(this.execution[this.execution.length - 1].state);
-    this.checkInvariants();
-    this.updateViews();
-  }
-
-  updateViews() {
-    this.views.forEach(view => view.update());
-  }
-}
 
 class TextStateView {
   constructor(controller, elem, module) {
