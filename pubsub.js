39da836524a758e93c0123566100dd98b152ca2e
Diego Ongaro <dongaro@salesforce.com>
2016-03-03 11:59:32 -0800

    pubsub.js: Add unit tests, docs, fix bugs

--- pubsub.js
+++ pubsub.js
@@ -2,34 +2,39 @@
 
 let _ = require('lodash');
 
+// Represents a source of events that multiple handlers can subscribe to.
 class PubSub {
   constructor() {
     this._handlers = [];
   }
 
-  pub() {
+  // Call the handlers in order with the arguments provided.
+  //
+  // If a handler removes one that has not yet been called, it won't be called
+  // here or again. If a handler adds another, it won't be called now but will
+  // be called in subsequent calls to pub().
+  pub(/* arguments */) {
     // The awkwardness here is to give decent semantics to sub()/unsub() calls
-    // done in handlers.
+    // done in handlers (a simple call to map() doesn't do the trick).
     let results = [];
-    _.clone(this._handlers).forEach((handler, i) => {
-      if (this._handlers.indexOf(handler) >= 0) {
-        results.push(handler(...arguments, i + 1));
+    _.clone(this._handlers).forEach(handler => {
+      if (_.indexOf(this._handlers, handler) >= 0) {
+        results.push(handler(...arguments));
       }
     });
     return results;
   }
 
+  // Appends 'cb' to the list of handlers to be called on future,
+  // not-yet-started invocations of pub().
   sub(cb) {
     this._handlers.push(cb);
-    return this._handlers.length;
   }
 
-  unsub(idOrCb) {
-    if (_.isFunction(idOrCb)) {
-      _.pull(this._handlers, idOrCb);
-    } else {
-      _.pull(this._handlers, idOrCb - 1);
-    }
+  // Removes the first occurrence of 'cb' from the list of handlers.
+  // It will no longer be called by pub().
+  unsub(cb) {
+    _.pullAt(this._handlers, _.indexOf(this._handlers, cb));
   }
 }
 
