a9598807bb0e32c2aa690f4fada77301d39d0ebd
Diego Ongaro <dongaro@salesforce.com>
2016-05-06 11:33:22 -0700

    Add MIT license and copyright headers

--- dist/models/counter/counter.jsx
+++ dist/models/counter/counter.jsx
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015-2016, Salesforce.com, Inc.
+ * All rights reserved.
+ * Licensed under the MIT license.
+ * For full license text, see LICENSE.md file in the repo root or
+ * https://opensource.org/licenses/MIT
+ */
+
 "use strict";
 
 let React = require('deprecated!react');
